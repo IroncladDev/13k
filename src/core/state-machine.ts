bc086dee2dbcfd4361c1485160d6c7a8e7b6fc56
Conner Ow <conner@connerow.dev>
2024-08-15 09:56:11 -0500

    customize

--- src/core/state-machine.ts
+++ src/core/state-machine.ts
@@ -1,20 +1,20 @@
-import { State } from './state';
+import { State } from "./state"
 
 export class StateMachine {
-  private currentState: State;
+    private currentState: State
 
-  constructor(initialState: State, ...enterArgs: any) {
-    this.currentState = initialState;
-    this.currentState.onEnter?.(...enterArgs);
-  }
+    constructor(initialState: State, ...enterArgs: any) {
+        this.currentState = initialState
+        this.currentState.onEnter?.(...enterArgs)
+    }
 
-  setState(newState: State, ...enterArgs: any) {
-    this.currentState.onLeave?.();
-    this.currentState = newState;
-    this.currentState.onEnter?.(...enterArgs);
-  }
+    setState(newState: State, ...enterArgs: any) {
+        this.currentState.onLeave?.()
+        this.currentState = newState
+        this.currentState.onEnter?.(...enterArgs)
+    }
 
-  getState() {
-    return this.currentState;
-  }
+    getState() {
+        return this.currentState
+    }
 }
